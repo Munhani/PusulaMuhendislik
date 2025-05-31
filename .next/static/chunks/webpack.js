15482864ec85881d2236f4414271b3ec43fac266
Pusula2006 <taylan@pusula.com>
2025-05-31 13:14:19 +0300

    chore: node_modules klasörünü git'ten kaldır

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b328c334184c7e17"; }
+/******/ 		__webpack_require__.h = function() { return "3946608a280750e2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
